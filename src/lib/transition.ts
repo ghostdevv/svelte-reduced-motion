1e62396cee2c329605373b942679ef8152cc3cca
GHOSt <ghostdevbusiness@gmail.com>
2021-12-11 18:46:40 +0000

    feat: createTransition

---
+++ src/lib/transition.ts
@@ -0,0 +1,23 @@
+import type { TransitionConfig } from 'svelte/transition';
+import { fade } from 'svelte/transition';
+import { reducedMotion } from './store';
+import { get } from 'svelte/store';
+
+export type Transition<T> = (node: Element, options?: T) => TransitionConfig;
+export type OptionsPair<T> = [transition: Transition<T>, options?: T];
+
+export type AcceptedTransition<T> = Transition<T> | OptionsPair<T>;
+
+export const createTransition =
+    <Base, Fallback>(
+        base: AcceptedTransition<Base>,
+        fallback: AcceptedTransition<Fallback> = fade,
+    ) =>
+    (node: Element, options: Base & Fallback): TransitionConfig => {
+        const run = (transition: AcceptedTransition<Base | Fallback>) =>
+            Array.isArray(transition)
+                ? transition[0](node, { ...transition[1], options })
+                : transition(node, options);
+
+        return get(reducedMotion) ? run(fallback) : run(base);
+    };
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,22 @@
 <script>
-    import { reducedMotion } from '$lib';
+    import { reducedMotion, createTransition } from '$lib';
+    import { fade, fly, draw } from 'svelte/transition';
+
+    let showing;
+
+    const accessibleTransition = createTransition([fly, { y: -20 }], draw);
 </script>
 
 <p>
     Reduced Motion: {$reducedMotion}
 </p>
+
+<hr />
+
+<button on:click={() => (showing = !showing)}> Toggle </button>
+
+{#if showing}
+    <div transition:accessibleTransition style="padding: 16px 0px;">
+        Hello World
+    </div>
+{/if}
