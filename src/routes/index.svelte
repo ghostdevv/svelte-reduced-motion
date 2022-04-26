13234a681912705fa69585411f8cf921ea385962
GHOST <ghostdevbusiness@gmail.com>
2022-04-26 13:19:09 +0100

    docs: update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,6 +11,12 @@
     Reduced Motion: {$reducedMotion}
 </p>
 
+<p>
+    You can test this in Chrome easily by opening dev tools, pressing the three
+    dots in the top right, then going on more tools > rendering and finding the
+    "Emulate css media feature prefers reduced motion"
+</p>
+
 <hr />
 
 <button on:click={() => (showing = !showing)}> Toggle Animation </button>
